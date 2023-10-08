1a0d4394fa74da71d6ae524289ed1122a2b1a724
Ramon Estol <ramonestolgarcia@gmail.com>
2023-10-08 09:33:19 +0200

    fixing domain errors

--- detectionApi.js
+++ detectionApi.js
@@ -1,72 +1,115 @@
-// Define las coordenadas iniciales
-const initialLat = 51.505;
-const initialLng = -0.09;
 let map = null;
 
 function initMap(lat, lng) {
-    if (map !== null) {
-        map.remove(); // Elimina el mapa existente si lo hay
-    }
-
-    map = L.map('map').setView([lat, lng], 13);
-    
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-}
-
-// Llama a la función initMap en DOMContentLoaded para cargar el mapa con coordenadas iniciales
-// document.addEventListener("DOMContentLoaded", function () {
-//     fetch('https://api.ipify.org?format=json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const ipAddress = data.ip;
-//     getLocation()
-//   })
-//   .catch(error => {
-//     console.error('Error al obtener la dirección IP:', error);
-//   });
-
-// });
-
-function getLocation() {
-    const inputIP = document.getElementById("IP").value;
-    const apiKey = 'at_Xlg4rXP9NOZbGh2pbivsYIbSXphfL'; // Reemplaza con tu propia API key de Geo IPify
-    const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${inputIP}`;
-
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `The request was not successful. Status: ${response.status}`
-          );
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const ipAddress = data.ip;
-        const location = data.location.city + ', ' + data.location.country + ' ' + data.location.postalCode;
-        const timezone = data.location.timezone;
-        const isp = data.isp;
-        const lat = data.location.lat;
-        const lng = data.location.lng;
-        console.log(lat,lng,ipAddress,location,timezone);
-        initMap(lat,lng);
-        document.getElementById("ipAddress").textContent = ipAddress;
-        document.getElementById("location").textContent = location;
-        document.getElementById("timezone").textContent = 'UTC '+timezone;
-        document.getElementById("isp").textContent = isp;
-        initMap(lat,lng);
-
-      })
-      .catch((error) => {
-        console.error("Error getting location:", error);
-      });
-
-
+  if (map !== null) {
+    map.remove(); // Remove the existing map if any
   }
 
-    const getLocationBtn = document.getElementById("getLocationBtn");
-    getLocationBtn.addEventListener("click", getLocation);
-  
+  map = L.map("map").setView([lat, lng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      const ipAddress = data.ip;
+      getLocation(ipAddress);
+      console.log(`IP Address: ${ipAddress}`);
+    })
+    .catch((error) => {
+      console.error("Error getting IP:", error);
+    });
+});
+
+function checkInputValue() {
+  const inputValue = document.getElementById("IP").value;
+  const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+  const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let inputIP = inputValue;
+
+  if (domainRegex.test(inputValue)) {
+    getIpFromDomain(inputValue)
+      .then((ipAddress) => {
+        console.log(`The IP address of ${inputValue} is: ${ipAddress}`);
+        getLocation(ipAddress);
+      });
+  } else if (!ipRegex.test(inputValue)) {
+    alert("You should enter a valid IP or domain.");
+  } else {
+    getLocation(inputIP);
+  }
+}
+
+function getIpFromDomain(domain) {
+  return fetch(`https://dns.google/resolve?name=${domain}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.Answer && data.Answer.length > 0) {
+        const ipAddress = data.Answer[0].data;
+        console.log(ipAddress)
+        return ipAddress; // Return the obtained IP address
+      } else {
+        throw new Error(`IP address not found for ${domain}`);
+      }
+    });
+}
+// function getIpFromDomain(domain) {
+//   return fetch(`https://dns.google/resolve?name=${domain}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data && data.Answer && data.Answer.length > 0) {
+//         const ipAddress = data.Answer[0].data;
+//         if (ipAddress !== domain) {
+//           // Si la IP obtenida es diferente del dominio, significa que hay una redirección
+//           console.log(`Redirection detected for ${domain}.`);
+//           // Llama recursivamente a la función para obtener la IP final
+//           return getIpFromDomain(ipAddress);
+//         }
+//         console.log(`IP address for ${domain} is: ${ipAddress}`);
+//         return ipAddress;
+//       } else {
+//         throw new Error(`IP address not found for ${domain}`);
+//       }
+//     });
+// }
+function getLocation(inputIP) {
+  console.log(inputIP);
+
+  const apiKey = "at_Xlg4rXP9NOZbGh2pbivsYIbSXphfL";
+  const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${inputIP}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const ipAddress = data.ip;
+      const location =
+        data.location.city +
+        ", " +
+        data.location.country +
+        " " +
+        data.location.postalCode;
+      const timezone = data.location.timezone;
+      const isp = data.isp;
+      const lat = data.location.lat;
+      const lng = data.location.lng;
+      console.log(lat, lng, ipAddress, location, timezone);
+      initMap(lat, lng);
+      document.getElementById("ipAddress").textContent = ipAddress;
+      document.getElementById("location").textContent = location;
+      document.getElementById("timezone").textContent = "UTC " + timezone;
+      document.getElementById("isp").textContent = isp;
+      initMap(lat, lng);
+    })
+    .catch((error) => {
+      console.error("Error getting location:", error);
+    });
+}
+
+const getLocationBtn = document.getElementById("getLocationBtn");
+getLocationBtn.addEventListener("click", checkInputValue);
