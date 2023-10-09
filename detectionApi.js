fc96190b5c91c856881bba1db6dc260620a5be67
Ramon Estol <ramonestolgarcia@gmail.com>
2023-10-09 09:06:38 +0200

    fixed bug after reload the page on init

--- detectionApi.js
+++ detectionApi.js
@@ -1,5 +1,11 @@
 let map = null;
 
+window.addEventListener("beforeunload", function () {
+  if (map !== null) {
+    map.remove();
+  }
+});
+
 function initMap(lat, lng) {
   if (map !== null) {
     map.remove(); // Remove the existing map if any
@@ -15,12 +21,12 @@ function initMap(lat, lng) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  
   fetch("https://api.ipify.org?format=json")
     .then((response) => response.json())
     .then((data) => {
       const ipAddress = data.ip;
       getLocation(ipAddress);
-      console.log(`IP Address: ${ipAddress}`);
     })
     .catch((error) => {
       console.error("Error getting IP:", error);
@@ -34,11 +40,9 @@ function checkInputValue() {
   let inputIP = inputValue;
 
   if (domainRegex.test(inputValue)) {
-    getIpFromDomain(inputValue)
-      .then((ipAddress) => {
-        console.log(`The IP address of ${inputValue} is: ${ipAddress}`);
-        getLocation(ipAddress);
-      });
+    getIpFromDomain(inputValue).then((ipAddress) => {
+      getLocation(ipAddress);
+    });
   } else if (!ipRegex.test(inputValue)) {
     alert("You should enter a valid IP or domain.");
   } else {
@@ -47,43 +51,29 @@ function checkInputValue() {
 }
 
 function getIpFromDomain(domain) {
+  const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+
   return fetch(`https://dns.google/resolve?name=${domain}`)
     .then((response) => response.json())
     .then((data) => {
       if (data && data.Answer && data.Answer.length > 0) {
         const ipAddress = data.Answer[0].data;
-        console.log(ipAddress)
-        return ipAddress; // Return the obtained IP address
+        if (!ipRegex.test(ipAddress)) {
+          //If the obtained IP is different from the domain, it means that there is a redirect          console.log(`Redirection detected for ${domain}.`);
+          // Recursively call the function to get the final IP
+          return getIpFromDomain(ipAddress);
+        }
+        return ipAddress;
       } else {
         throw new Error(`IP address not found for ${domain}`);
       }
     });
 }
-// function getIpFromDomain(domain) {
-//   return fetch(`https://dns.google/resolve?name=${domain}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       if (data && data.Answer && data.Answer.length > 0) {
-//         const ipAddress = data.Answer[0].data;
-//         if (ipAddress !== domain) {
-//           // Si la IP obtenida es diferente del dominio, significa que hay una redirección
-//           console.log(`Redirection detected for ${domain}.`);
-//           // Llama recursivamente a la función para obtener la IP final
-//           return getIpFromDomain(ipAddress);
-//         }
-//         console.log(`IP address for ${domain} is: ${ipAddress}`);
-//         return ipAddress;
-//       } else {
-//         throw new Error(`IP address not found for ${domain}`);
-//       }
-//     });
-// }
-function getLocation(inputIP) {
-  console.log(inputIP);
 
+function getLocation(inputIP) {
   const apiKey = "at_Xlg4rXP9NOZbGh2pbivsYIbSXphfL";
   const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${inputIP}`;
-
+  
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -104,7 +94,6 @@ function getLocation(inputIP) {
       document.getElementById("location").textContent = location;
       document.getElementById("timezone").textContent = "UTC " + timezone;
       document.getElementById("isp").textContent = isp;
-      initMap(lat, lng);
     })
     .catch((error) => {
       console.error("Error getting location:", error);
